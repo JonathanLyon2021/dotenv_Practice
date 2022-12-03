a0c536158aaa057cb2d5ed47d11a24ce6a6e9eed
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-02 23:57:07 -0500

    Create main.js

---
+++ newFile/main.js
@@ -0,0 +1,7 @@
+require("dotenv").config(); //.config() - reads the environment variables from .env file and adds them to process.env
+
+var heading = "Jonathan's Wallet";
+var balance1 = "Your current wallet balance is: ".concat(process.env.WALLET_BALANCE);
+var privKey2 = "Your private key is:: ".concat(process.env.PRIVATE_KEY);
+
+console.log(heading, balance1, privKey2); // process.env - is an object that contains all the environment variables
